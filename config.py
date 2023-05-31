3304534b634bbc663a7f9998952fc886f2acc1d0
Iain Watt <iainskwatt@gmail.com>
2023-05-31 16:09:35 +0100

    just a quick update

--- config.py
+++ config.py
@@ -18,4 +18,6 @@ class Config(object):
     POSTS_PER_PAGE = 5
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or \
+        ['http://localhost:9200']
 
